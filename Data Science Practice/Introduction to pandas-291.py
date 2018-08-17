fe8845090a2aa975cb186050a89f674b4101d9a8
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-08-17 21:44:17 +0530

    indexing and slicing with pandas

--- Data Science Practice/Introduction to pandas-291.py
+++ Data Science Practice/Introduction to pandas-291.py
@@ -20,3 +20,33 @@ industries = f500.loc[:, "industry"]
 previous = f500.loc[:, ["rank", "previous_rank", "years_on_global_500_list"]]
 
 financial_data = f500.loc[:, "revenues" : "profit_change"]
+
+## 4. Column selection shortcuts ##
+
+countries = f500["country"]
+revenues_years = f500[["revenues", "years_on_global_500_list"]]
+ceo_to_sector = f500.loc[:, "ceo" : "sector"]
+
+## 5. Selecting Items from a Series by Label ##
+
+ceos = f500["ceo"]
+
+walmart = ceos["Walmart"]
+apple_to_samsung = ceos["Apple" : "Samsung Electronics"]
+
+oil_companies = ceos[["Exxon Mobil", "BP", "Chevron"]]
+
+## 6. Selecting Rows From a DataFrame by Label ##
+
+drink_companies = f500.loc[["Anheuser-Busch InBev", "Coca-Cola", "Heineken Holding"]]
+
+big_movers = f500.loc[["Aviva", "HP", "JD.com", "BHP Billiton"],["rank", "previous_rank"]]
+
+middle_companies = f500.loc["Tata Motors": "Nationwide", "rank": "country"]
+
+## 7. Series and Dataframe Describe Methods ##
+
+profits_desc = f500["profits"].describe()
+revenue_and_employees_desc = f500[["revenues", "employees"]].describe()
+
+all_desc = f500.describe(include='all')
